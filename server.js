72a829793c5598cbea30b4059b864010d76aa214
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-04 17:39:11 -0500

    adds cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const accountRouter = require('./routers/accountRouter')
-
+const cors = require('cors')
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/accounts', accountRouter);
+server.use(cors());
 
 
 module.exports = server;
