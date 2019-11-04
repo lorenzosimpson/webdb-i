48491ea9ce947e54ccf0ed1887bda00ff4dabb4a
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-04 16:31:47 -0500

    adds successful get and posts

--- routers/accountRouter.js
+++ routers/accountRouter.js
@@ -1,4 +1,23 @@
 const express = require('express');
-const knex = require('knex');
+const knex = require('../data/dbConfig.js');
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    knex
+    .select('*')
+    .from('accounts')
+    .then(accounts => res.status(200).json(accounts))
+    .catch(err => res.status(500).json({ error: 'Failed to get accounts' }))
+})
+
+router.post('/', (req, res) => {
+    knex
+    .insert(req.body, 'id')
+    .into('accounts')
+    .then(newAccount => res.status(201).json(newAccount))
+    .catch(err => res.status(500).json({ error: 'Failed to add new account' }))
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const accountRouter = require('./routers/accountRouter')
 
-const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountRouter);
+
 
 module.exports = server;
